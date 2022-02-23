5023ac47ca406f2a37f76a48287c85587ff0b3d8
Zach <zmriel@gmail.com>
2022-02-23 15:52:39 -0500

    implement custom getLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
 import Layout from "../src/components/Layout";
 import theme from "../theme";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
     </ChakraProvider>
   );
 }
