9bfa8d6d01bd701fbbfe6b76576f401e541e539c
Zach <zmriel@gmail.com>
2022-09-23 11:06:31 -0400

    chore: cleanup

--- packages/ui/src/components/Footer.tsx
+++ packages/ui/src/components/Footer.tsx
@@ -3,42 +3,23 @@ import { ComponentPropsWithoutRef } from 'react';
 
 function FooterContent() {
   return (
-    <div
-    // w="100%"
-    // h="100%"
-    // align="center"
-    // justify="flex-end"
-    // gap={3}
-    // color="gray.400"
-    // maxW="1100px"
-    >
+    <div>
       <a
         aria-label="go to Zach's github profile"
         href={siteConfig.github.profile}
         target="_blank"
         rel="noreferrer"
       >
-        {/* <Icon
-          as={GitHubIcon}
-          display="block"
-          transition="color 0.3s"
-          _hover={{ color: 'gray.500' }}
-          boxSize="5"
-        /> */}
         GitHubIcon
       </a>
-      {/* <ColorModeSwitch /> */}
     </div>
   );
 }
 
 export default function Footer(props: ComponentPropsWithoutRef<'footer'>) {
-  // const bgColor = useColorModeValue('white', 'gray.800');
-
   return (
     <footer
       style={{
-        // bgColor:{bgColor}
         bottom: 0,
         left: 0,
         position: 'sticky',
