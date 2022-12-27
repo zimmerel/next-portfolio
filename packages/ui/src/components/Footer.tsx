5e11293f1c16fc579a93ee1ce66cd801752a890a
Zach <zmriel@gmail.com>
2022-12-27 10:45:37 -0500

    chore: update deps

--- packages/ui/src/components/Footer.tsx
+++ packages/ui/src/components/Footer.tsx
@@ -1,12 +1,16 @@
-import { siteConfig } from 'configs';
+import { resume } from 'profile';
 import { ComponentPropsWithoutRef } from 'react';
+
+const GITHUB_PROFILE = resume.basics.profiles.find((p) =>
+  /github/i.test(p.network)
+);
 
 function FooterContent() {
   return (
     <div>
       <a
         aria-label="go to Zach's github profile"
-        href={siteConfig.github.profile}
+        href={GITHUB_PROFILE?.network}
         target="_blank"
         rel="noreferrer"
       >
